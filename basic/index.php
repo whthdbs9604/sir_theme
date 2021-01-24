bedbd333c69b05b34cb502ffec786e8612a00266
whthdbs9604 <75105238+whthdbs9604@users.noreply.github.com>
2021-01-25 00:35:20 +0900

    UP

--- basic/index.php
+++ basic/index.php
@@ -17,7 +17,7 @@ include_once(G5_THEME_PATH.'/head.php');
 
 <script>
     $(document).ready(function(){
-        $(".bxsdr").bxSlider({auto:true});
+        $(".bxsdr").bxSlider({auto:true,controls:false,pager:false});
     });
 </script>
 
@@ -28,6 +28,19 @@ include_once(G5_THEME_PATH.'/head.php');
         <li><img src="<? echo G5_THEME_IMG_URL ?>/pc03.jpg" alt="1111"></li>
     </ul>
 </div>
+
+<div class="latest_wr">
+    <!-- 공지사항 { -->
+    <?php
+    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
+    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
+    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+    echo latest('theme/basic', 'bbs_notice', 3, 30);      // 최소설치시 자동생성되는 갤러리게시판
+    ?>
+    <!-- } 공지사항 끝 -->
+</div>
+
+
 
 <?php
 include_once(G5_THEME_PATH.'/tail.php');
